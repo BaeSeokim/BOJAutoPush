5edd99069b9c4a2af81cdf628b3d5774a4c51dd5
Bae Seok <qotjr0151@gmail.com>
2024-02-06 15:10:27 +0900

    [Silver V] Title: 숫자 카드, Time: 3260 ms, Memory: 113340 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+a = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+b = int(input())
+lst_ch = list(map(int, input().split()))
+
+for m in lst_ch:
+    left = 0
+    right = a - 1
+    arr = []
+    while left <= right:
+        mid = (left + right) // 2
+        if m > lst[mid]:
+            left = mid + 1
+        elif m < lst[mid]:
+            right = mid -1
+        else:
+            left = mid
+            right = mid
+            break
+    if left == mid and right == mid:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
