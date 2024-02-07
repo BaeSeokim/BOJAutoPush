eee27f05a9409493b07bb40a197c322cd54297ad
Bae Seok <qotjr0151@gmail.com>
2024-02-07 17:27:03 +0900

    [Bronze III] Title: 약수 구하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(int, input().split())
+arr = []
+for i in range(1, a+1):
+    if (a % i) == 0:
+        arr.append(i)
+if len(arr) < b:
+    print(0)
+else:
+    print(arr[b-1])
